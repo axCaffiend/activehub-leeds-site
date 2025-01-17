964423f168f89b4cbf6db0c729c5231b56db1a9d
axCaffiend <axfulcher@outlook.com>
2025-01-17 05:02:37 +0000

    Added templates files for Events, archive, single (detail) and content (template part).

---
+++ themes/activehub-leeds/template-parts/content-event.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package activehub-leeds
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+		the_title( '<h1 class="entry-title">', '</h1>' );
+		if ( has_post_thumbnail()) :
+			the_post_thumbnail();
+		endif?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+		the_content();
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
